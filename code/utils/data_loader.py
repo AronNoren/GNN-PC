a7d2b8d06fb7a394a0caf6fd215055ee90ddca1b
AronNoren <norenaron@gmail.com>
2020-12-07 10:57:47 +0100

    minor fixes

--- code/utils/data_loader.py
+++ code/utils/data_loader.py
@@ -1,10 +1,19 @@
-data_loader(root = 'data/ShapeNet',categories = None,points = None):
+import sys
+import os
+parent_dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(parent_dir_path)
+from utils.node2graph import node2graph
+from torch_geometric.datasets import ShapeNet
+def data_loader(root = 'data/ShapeNet',categories = None,points = None):
 	'''
-	loads the ShapeNet dataset and created graph labels from node segment labeling.
-	Input: root, directory of Dataset
+	loads/downloads the ShapeNet dataset and created graph labels from node segment labeling.
+	Input: 
+		root, directory of Dataset
+		categories, list of categories to include e.g. ['Airplane','Car']. None for all.
+		points, specified number of points in pointcloud. None for all avaliable points.
 	'''
-	if points None:
-		dataset = ShapeNet(root,categories)
+	if points is not None:
+		dataset = ShapeNet(root,categories,transform = FixedPoints(num=points))
 	else:
-		dataset = ShapeNet(root, categories,transform = FixedPoints(num=points))
+		dataset = ShapeNet(root, categories)
 	return node2graph(dataset)
--- code/utils/node2graph.py
+++ code/utils/node2graph.py
@@ -1,3 +1,7 @@
+import sys
+import os
+parent_dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(parent_dir_path)
 def node2graph(dataset):
 
     '''
