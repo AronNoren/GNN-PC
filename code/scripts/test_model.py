9960123307c522d846bdd4b738630ca5631400e7
AronNoren <norenaron@gmail.com>
2020-12-08 14:08:14 +0100

    testing function for functionalities in branch

--- code/scripts/test_model.py
+++ code/scripts/test_model.py
@@ -6,51 +6,13 @@ import torch
 from torch_geometric.data import DataLoader
 from utils.data_loader import data_loader
 from models.PointNet import get_model
-
+from utils.training import train_PN
+from utils.evaluation import evaluate_PN
 dataset = data_loader(root = 'data/ShapeNet',categories = ['Airplane','Bag','Cap','Car','Chair','Earphone','Guitar'])
-train_loader = DataLoader(dataset[:int(4*len(dataset)/5)], batch_size=10) #0.8/0.2 train/test-split
-test_loader = DataLoader(dataset[int(4*len(dataset)/5):], batch_size=10)
-model = get_model(7)
-SAVEPATH = 'code/models/saved_models/net_params_7.pkl'
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-weight = torch.tensor([1.0,35.0,50.0,3.0,0.72,39.0,3.43])
-criterion = torch.nn.CrossEntropyLoss(weight)  # Define loss criterion.
-print("data loaded")
-def train(model, optimizer, loader):
-    model.train()
-    
-    total_loss = 0
-    for data in train_loader:
-        print(data.y)
-        optimizer.zero_grad()  # Clear gradients.
-        logits = model(data.pos, data.batch)  # Forward pass.
-        loss = criterion(logits, data.y)  # Loss computation.
-
-        loss.backward()  # Backward pass.
-        optimizer.step()  # Update model parameters.
-        total_loss += loss.item() * data.num_graphs
-
-    return total_loss / len(train_loader.dataset)
-
-
-torch.no_grad()
-def test(model, loader):
-    model.eval()
-
-    total_correct = 0
-    for data in loader:
-        logits = model(data.pos, data.batch)
-        pred = logits.argmax(dim=-1)
-        print(pred)
-        total_correct += int((pred == data.y).sum())
-
-    return total_correct / len(loader.dataset)
-
-for epoch in range(1, 3):
-    loss = train(model, optimizer, train_loader)
-    test_acc = test(model, test_loader)
-    print(f'Epoch: {epoch:02d}, Loss: {loss:.4f}, Test Accuracy: {test_acc:.4f}')
-torch.save(model.state_dict(), SAVEPATH)
-model = get_model(7)
+traindata = dataset[:int(4*len(dataset)/5)]
+testdata = dataset[int(4*len(dataset)/5):]
+train_PN(traindata,n_epochs = 5)
+SAVEPATH = 'code/models/saved_models/model.pkl'
+model = get_model(21)
 model.load_state_dict(torch.load(SAVEPATH))
-model.eval()
+print(evaluate_PN(model,testdata))
