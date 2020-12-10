5d79296654099c57be27ca0d26f0d15cdc0eb77e
Arek Sahal <areksahal@Areks-MacBook-Air.local>
2020-12-10 16:03:36 +0100

    added PPFNet training funciton

---
+++ code/scripts/train_PPFNet.py
@@ -0,0 +1,29 @@
+import sys
+import os
+parent_dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(parent_dir_path)
+import torch
+from utils.data_loader import get_ShapeNet
+from utils.training import train_PPFNet
+from utils.evaluation import evaluate_PN
+from utils.transform import transform
+from utils.visualizations import scatterplot
+
+# train model on ordinary dataset
+traindata, testdata = get_ShapeNet(root = 'data/ShapeNet',transformation = transform(points = 256))
+
+model = train_PPFNet(traindata,n_epochs = 10)
+SAVEPATH = 'code/models/saved_models/new_PPFNetmodel.pkl'
+torch.save(model.state_dict(), SAVEPATH)
+
+#train model on randomly rotated dataset
+rotated_traindata, rotated_testdata = get_ShapeNet(root = 'data/ShapeNet',transformation = transform(points = 256,rotate = 180))
+
+rotated_model = train_PN(rotated_traindata,n_epochs = 10)
+rotated_SAVEPATH = 'code/models/saved_models/new_PPFNetmodel_rotate.pkl'
+torch.save(rotated_model.state_dict(), rotated_SAVEPATH)
+
+#evaluate model
+print('Acc no rotations on training nor testdata: ' + str(evaluate_PN(model,testdata)))
+print('Acc no rotations on training but rotation on testdata: ' + str(evaluate_PN(model,rotated_testdata)))
+print('Acc rotations on training and testdata: ' + str(evaluate_PN(rotated_model,rotated_testdata)))
--- code/utils/training_PPFNet.py
+++ code/utils/training_PPFNet.py
@@ -4,8 +4,7 @@ parent_dir_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 sys.path.append(parent_dir_path)
 import torch
 from torch_geometric.data import DataLoader
-import models.PointNet
-import models.PPFNet
+from models.PPFNet import get_model
 
 def train_PN(train_dataset,validation_dataset = None,batchsize = 10,n_epochs =5,shuffled = True,weight = False):
     '''
@@ -21,56 +20,7 @@ def train_PN(train_dataset,validation_dataset = None,batchsize = 10,n_epochs =5,
     #SAVEPATH = 'code/models/saved_models/' + SAVENAME + '.pkl'
     n_classes = train_dataset.num_classes
     train_loader = DataLoader(train_dataset, batch_size=batchsize,shuffle = shuffled)
-    model = PointNet.get_model(n_classes)
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-    if weight is not False:
-        weight = torch.bincount(train_dataset.data.y) #count number of each label
-        weight = torch.true_divide(torch.max(weight),weight)
-        criterion = torch.nn.CrossEntropyLoss(weight)  # Define loss criterion.
-    else:
-        criterion = torch.nn.CrossEntropyLoss()
-
-    def train(model, optimizer, train_loader):
-        model.train()
-    
-        total_loss = 0
-        for data in train_loader:
-            #print(data.y)
-            optimizer.zero_grad()  # Clear gradients.
-            logits = model(data.pos, data.batch, data.x)  # Forward pass.
-            loss = criterion(logits, data.y)  # Loss computation.
-
-            loss.backward()  # Backward pass.
-            optimizer.step()  # Update model parameters.
-            total_loss += loss.item() * data.num_graphs
-
-        return total_loss / len(train_loader.dataset)
-
-    for epoch in range(1, n_epochs):
-        loss = train(model, optimizer, train_loader)
-        val_acc = 0
-        if validation_dataset is not None:
-        	val_acc = evaluation(model, test_loader)
-        print(f'Epoch: {epoch:02d}, Loss: {loss:.4f}, val acc:{val_acc:.4f}')
-    
-    return model
-
-
-def train_PPFNet(train_dataset,validation_dataset = None,batchsize = 10,n_epochs =5,shuffled = True,weight = False):
-    '''
-    Training function for PointNet, returns a trained model.
-    Input:
-    train_dataset (dataset) pointcloud data for training
-    validation_dataset (dataset) optional pointcloud data for validation acc during training
-    batchsize = 10 (int) nr of data from train_dataset to process in parallell.
-    n_epochs = 5 (int) nr of times the train_dataset is trained on.
-    shuffled = True (bool) shuffles train_dataset before dividing into batches.
-    weight = False (bool) used for uneven dataset to 
-    '''
-    #SAVEPATH = 'code/models/saved_models/' + SAVENAME + '.pkl'
-    n_classes = train_dataset.num_classes
-    train_loader = DataLoader(train_dataset, batch_size=batchsize,shuffle = shuffled)
-    model = PPFNet.get_model(n_classes)
+    model = get_model(n_classes)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
     if weight is not False:
         weight = torch.bincount(train_dataset.data.y) #count number of each label
